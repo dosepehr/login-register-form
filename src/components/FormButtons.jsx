a7fd84f6719fb85b87afd0be8d665acf28f2566d
dosepehr <dosepehr@gmail.com>
2023-03-16 02:21:13 +0330

    add buttons style

--- src/components/FormButtons.jsx
+++ src/components/FormButtons.jsx
@@ -11,16 +11,27 @@ const FormButtons = () => {
     return (
         <>
             <div
-                className='flex  my-9 items-center justify-center rounded-3xl shadow-lg shadow-[#ff4d4d4d]'
+                className='flex my-9 items-center justify-center rounded-3xl shadow-lg shadow-[#ff4d4d4d]'
                 onClick={changeForm}
             >
-                <div id='btn'></div>
-                <button className='bg-transparent border-none py-2 px-9 cursor-pointer outline-none relative duration-300'>
-                    login
-                </button>
-                <button className='bg-transparent border-none py-2 px-9 cursor-pointer outline-none relative duration-300'>
-                    register
-                </button>
+                <div className='relative'>
+                    <div
+                        className={`bg-gradient-to-r from-[#f3446a] to-[#ff6464] h-full absolute rounded-full duration-300 w-40 top-0 
+                        
+                        ${
+                            currentForm === 'login'
+                                ? 'right-[110px]'
+                                : '-right-10'
+                        }
+                        `}
+                    ></div>
+                    <button className='bg-transparent border-none py-2 px-9 cursor-pointer outline-none relative duration-300'>
+                        register
+                    </button>
+                    <button className='bg-transparent border-none py-2 px-9 cursor-pointer outline-none relative duration-300'>
+                        login
+                    </button>
+                </div>
             </div>
         </>
     );
