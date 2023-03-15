62056a0c7759e91e7684ce1f753210a06a0c2195
dosepehr <dosepehr@gmail.com>
2023-03-15 23:46:19 +0330

    add app bg iamge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-    return <h1 className='text-3xl font-bold underline text-red-500'>Hello world!</h1>;
+  return (
+    <div className="app" >
+      
+      </div>
+    )
 }
 
 export default App;
