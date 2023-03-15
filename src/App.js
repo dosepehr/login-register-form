f62967b3eb6ef9fea53488a5aec8c2e7c74ee77e
dosepehr <dosepehr@gmail.com>
2023-03-16 00:46:01 +0330

    add login and register buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { LoginForm } from './components';
 function App() {
     return (
-        <div className='app absolute bg-center bg-cover h-full w-full'>
+        <div className='app absolute bg-center bg-cover h-full w-full flex items-center justify-center'>
             <LoginForm />
         </div>
     );
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const LoginForm = () => {
-    return <div>LoginForm</div>;
+    return (
+        <div>
+            <div className='w-96 h-96 bg-white rounded-md border px-10'>
+                <div class='flex  my-9 items-center justify-center rounded-3xl shadow-lg shadow-[#ff4d4d4d]'>
+                    <div id="btn"></div>
+                    <button class='bg-transparent border-none py-2 px-9 cursor-pointer outline-none relative duration-300'>login</button>
+                    <button class='bg-transparent border-none py-2 px-9 cursor-pointer outline-none relative duration-300'>register</button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default LoginForm;
