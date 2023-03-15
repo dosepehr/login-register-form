ad22650048d819a94263f50f1d7f8432db742330
dosepehr <dosepehr@gmail.com>
2023-03-16 01:36:45 +0330

    add context api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import { MainForm } from './components';
 import { useState } from 'react';
+import { mainContext } from './context';
 function App() {
-    const [currentForm, setCurrentForm] = useState();
+    const [currentForm, setCurrentForm] = useState('login');
     return (
-        <div className='app absolute bg-center bg-cover h-full w-full flex items-center justify-center'>
-            <MainForm
-                currentForm={currentForm}
-                setCurrentForm={setCurrentForm}
-            />
-        </div>
+        <mainContext.Provider
+            value={{
+                currentForm,
+                setCurrentForm,
+            }}
+        >
+            <div className='app absolute bg-center bg-cover h-full w-full flex items-center justify-center'>
+                <MainForm
+                    currentForm={currentForm}
+                    setCurrentForm={setCurrentForm}
+                />
+            </div>
+        </mainContext.Provider>
     );
 }
 
 export default App;
-
