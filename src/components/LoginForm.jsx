7b5c48a7343b35782500861bf22c34fed6cfae4f
dosepehr <dosepehr@gmail.com>
2023-03-16 17:42:38 +0330

    use formik for login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,25 +1,22 @@
 import { useContext } from 'react';
 import { mainContext } from '../context';
 import { useNavigate } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { LoginSchema } from '../validation/userSchema';
 const LoginForm = () => {
     const navigate = useNavigate();
-    const { currentForm, userInfo, setUserInfo, users, setUser } =
-        useContext(mainContext);
+    const { currentForm, users, setUser } = useContext(mainContext);
     // ! getting user info
-    const setContactInfoHandler = (e) => {
-        setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
-    };
-    const getUser = async (e) => {
-        e.preventDefault();
+    const getUser = async (values) => {
         try {
             // ! getting requested user and check its username and password
             const requestedUser = users.filter(
                 (user) =>
                     user.username.toLowerCase() ===
-                    userInfo.username.toLowerCase()
+                    values.username.toLowerCase()
             )[0];
             if (requestedUser) {
-                if (requestedUser.password === userInfo.password) {
+                if (requestedUser.password === values.password) {
                     navigate('/main');
                     setUser(requestedUser);
                 } else {
@@ -36,34 +33,48 @@ const LoginForm = () => {
     return (
         <>
             {/* if user requested for login form ,it will slide from left */}
-            <form
-                onSubmit={(e) => getUser(e)}
-                action=''
-                className={`flex flex-col space-y-4 absolute w-[19rem]
+            <Formik
+                initialValues={{
+                    username: '',
+                    password: '',
+                }}
+                validationSchema={LoginSchema}
+                onSubmit={(values) => {
+                    getUser(values);
+                }}
+            >
+                <Form
+                    className={`flex flex-col space-y-2 absolute w-[19rem]
                 duration-300
                 ${currentForm === 'login' ? 'left-full' : 'left-9'}
                 `}
-            >
-                <input
-                    onChange={setContactInfoHandler}
-                    name='username'
-                    type='text'
-                    placeholder='username'
-                    className='p-2 border border-[#777] rounded-3xl'
-                />
-                <input
-                    onChange={setContactInfoHandler}
-                    name='password'
-                    type='password'
-                    placeholder='password'
-                    className='p-2 border border-[#777] rounded-3xl'
-                />
-                <input
-                    type='submit'
-                    value='Login'
-                    className='text-[#333] text-lg rounded-3xl p-2 bg-gradient-to-r from-[#f3446a] to-[#ff6464]'
-                />
-            </form>
+                >
+                    <Field
+                        name='username'
+                        type='text'
+                        placeholder='username'
+                        className='p-2 border border-[#777] rounded-3xl'
+                    />
+                    <span className='text-red-500'>
+                        <ErrorMessage name='username' />
+                    </span>
+                    <Field
+                        name='password'
+                        type='password'
+                        placeholder='password'
+                        className='p-2 border border-[#777] rounded-3xl'
+                    />
+                    <span className='text-red-500'>
+                        <ErrorMessage name='password' />
+                    </span>
+                    <button
+                        type='submit'
+                        className='text-[#333] text-lg rounded-3xl p-2 bg-gradient-to-r from-[#f3446a] to-[#ff6464]'
+                    >
+                        Login
+                    </button>
+                </Form>
+            </Formik>
         </>
     );
 };
