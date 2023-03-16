74de0acdced268918c92b80c315d4d0ae86a2bf5
dosepehr <dosepehr@gmail.com>
2023-03-16 14:28:59 +0330

    add Login functionality

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,12 +1,43 @@
 import { useContext } from 'react';
 import { mainContext } from '../context';
+import { useNavigate } from 'react-router-dom';
 const LoginForm = () => {
-    const { currentForm } = useContext(mainContext);
+    const navigate = useNavigate();
+    const { currentForm, userInfo, setUserInfo, users, setUser } =
+        useContext(mainContext);
+    // ! getting user info
+    const setContactInfoHandler = (e) => {
+        setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
+    };
+    const getUser = async (e) => {
+        e.preventDefault();
+        try {
+            // ! getting requested user and check its username and password
+            const requestedUser = users.filter(
+                (user) =>
+                    user.username.toLowerCase() ===
+                    userInfo.username.toLowerCase()
+            )[0];
+            if (requestedUser) {
+                if (requestedUser.password === userInfo.password) {
+                    navigate('/main');
+                    setUser(requestedUser);
+                } else {
+                    console.log('wrong pass or username');
+                }
+            } else {
+                console.log('wrong pass or username');
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     return (
         <>
             {/* if user requested for login form ,it will slide from left */}
             <form
+                onSubmit={(e) => getUser(e)}
                 action=''
                 className={`flex flex-col space-y-4 absolute w-[19rem]
                 duration-300
@@ -14,18 +45,22 @@ const LoginForm = () => {
                 `}
             >
                 <input
+                    onChange={setContactInfoHandler}
+                    name='username'
                     type='text'
                     placeholder='username'
                     className='p-2 border border-[#777] rounded-3xl'
                 />
                 <input
+                    onChange={setContactInfoHandler}
+                    name='password'
                     type='password'
                     placeholder='password'
                     className='p-2 border border-[#777] rounded-3xl'
                 />
                 <input
                     type='submit'
-                    value='submit'
+                    value='Login'
                     className='text-[#333] text-lg rounded-3xl p-2 bg-gradient-to-r from-[#f3446a] to-[#ff6464]'
                 />
             </form>
