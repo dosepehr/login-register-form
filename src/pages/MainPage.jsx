bf614e4a6fb672ac889d8027601df8320a261d7a
dosepehr <dosepehr@gmail.com>
2023-03-16 04:52:37 +0330

    add logout function

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,14 +1,34 @@
 import { useContext } from 'react';
 import { mainContext } from '../context';
 const MainPage = () => {
-    const { user } = useContext(mainContext);
+    const { user, setUser } = useContext(mainContext);
+    const logOutHandler = () => {
+        setUser({});
+    };
     return (
         <div>
-            <p>
-                user : <span className='text-green-500 mx-2'>{user.username}</span>{' '}
-                is logged in with email
-                <span className='text-sky-500 mx-2'>{user.email}</span>
-            </p>
+            {Object.keys(user).length > 0 ? (
+                <>
+                    <p>
+                        user :
+                        <span className='text-green-500 mx-2'>
+                            {user.username}
+                        </span>
+                        is logged in with email
+                        <span className='text-sky-500 mx-2'>{user.email}</span>
+                    </p>
+                    <button
+                        className='bg-pink-500 text-white p-2'
+                        onClick={logOutHandler}
+                    >
+                        logOut
+                    </button>
+                </>
+            ) : (
+                <h1 className='bg-purple-500 text-center'>
+                    you are logged out !
+                </h1>
+            )}
         </div>
     );
 };
