f5e292bd14e884b6eca8ad5a65da5814f3af837e
dosepehr <dosepehr@gmail.com>
2023-03-16 03:52:23 +0330

    add register user service

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -6,3 +6,7 @@ export const getUsers = () => {
     const url = `${BASE_URL}/users`;
     return axios.get(url);
 };
+export const registerUser = (newUserData) => {
+    const url = `${BASE_URL}/users`;
+    axios.post(url, newUserData);
+};
