e3c03ebbd2d39fbde93c2a99b00484a4fa22ff70
dosepehr <dosepehr@gmail.com>
2023-03-16 15:41:39 +0330

    use formik for register form

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -2,20 +2,17 @@ import { useContext } from 'react';
 import { mainContext } from '../context';
 import { registerUser } from '../services/fetchData';
 import { useNavigate } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { userSchema } from '../validation/userSchema';
 const RegisterForm = () => {
     const navigate = useNavigate();
-    const { currentForm, userInfo, setUserInfo, setUser } =
-        useContext(mainContext);
+    const { currentForm, setUser } = useContext(mainContext);
 
     // ! getting new user info
-    const setUserInfoHandler = (e) => {
-        setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
-    };
-    const createUser = async (e) => {
-        e.preventDefault();
+    const createUser = async (values) => {
         try {
-            await registerUser(userInfo);
-            setUser(userInfo);
+            await registerUser(values);
+            setUser(values);
             navigate('/main');
         } catch (err) {
             console.log(err);
@@ -25,41 +22,58 @@ const RegisterForm = () => {
     return (
         <>
             {/* if user requested for register form ,it will slide from right */}
-            <form
-                onSubmit={(e) => createUser(e)}
-                action=''
-                className={`flex flex-col space-y-4 absolute w-[19rem]
+            <Formik
+                initialValues={{
+                    username: '',
+                    email: '',
+                    password: '',
+                }}
+                validationSchema={userSchema}
+                onSubmit={(values) => {
+                    createUser(values);
+                }}
+            >
+                <Form
+                    className={`flex flex-col space-y-2 absolute w-[19rem]
                 duration-300
                 ${currentForm === 'register' ? 'right-full' : 'right-9'}
                 `}
-            >
-                <input
-                    name='username'
-                    onChange={setUserInfoHandler}
-                    type='text'
-                    placeholder='username'
-                    className='p-2 border border-[#777] rounded-3xl'
-                />
-                <input
-                    name='email'
-                    onChange={setUserInfoHandler}
-                    type='email'
-                    placeholder='email'
-                    className='p-2 border border-[#777] rounded-3xl'
-                />
-                <input
-                    name='password'
-                    onChange={setUserInfoHandler}
-                    type='password'
-                    placeholder='password'
-                    className='p-2 border border-[#777] rounded-3xl'
-                />
-                <input
-                    type='submit'
-                    value='submit'
-                    className='text-[#333] text-lg rounded-3xl p-2 bg-gradient-to-r from-[#f3446a] to-[#ff6464]'
-                />
-            </form>
+                >
+                    <Field
+                        name='username'
+                        type='text'
+                        placeholder='username'
+                        className='p-2 border border-[#777] rounded-3xl'
+                    />
+                    <span className='text-red-500'>
+                        <ErrorMessage name='username' />
+                    </span>
+                    <Field
+                        name='email'
+                        type='text'
+                        placeholder='email'
+                        className='p-2 border border-[#777] rounded-3xl'
+                    />
+                    <span className='text-red-500'>
+                        <ErrorMessage name='email' />
+                    </span>
+                    <Field
+                        name='password'
+                        type='password'
+                        placeholder='password'
+                        className='p-2 border border-[#777] rounded-3xl'
+                    />
+                    <span className='text-red-500'>
+                        <ErrorMessage name='password' />
+                    </span>
+                    <button
+                        type='submit'
+                        className='text-[#333] text-lg rounded-3xl p-2 bg-gradient-to-r from-[#f3446a] to-[#ff6464]'
+                    >
+                        Register
+                    </button>
+                </Form>
+            </Formik>
         </>
     );
 };
