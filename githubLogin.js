fbd94c6ab52e2642260ee65655306da1e91c384b
dosepehr <dosepehr@gmail.com>
2023-03-17 01:34:44 +0330

    add githubLogin server

---
+++ githubLogin.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const fetch = (...args) =>
+    import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const bodyParser = require('body-parser');
+
+const CLIENT_ID = 'e197d9e5999efbb20ba4';
+const SECRET_ID = 'b144845a0a725ea42e753ef08b65e945b8e1393f';
+
+const server = express();
+server.use(cors());
+server.use(bodyParser.json());
+
+server.get('/getAccessToken', async function (req, res) {
+    const params = `?client_id=${CLIENT_ID}&client_secret=${SECRET_ID}&code=${req.query.code}`;
+    const response = await fetch(
+        `https://github.com/login/oauth/access_token${params}`,
+        {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+            },
+        }
+    );
+    const data = await response.json();
+    console.log(data);
+    // const datas = await response.json(data);
+});
+
+server.get('/getUserData', async function (req, res) {
+    req.get('Authorization');
+    const response = await fetch('https://api.github.com/user', {
+        headers: {
+            Authorization: req.get('Authorization'),
+        },
+    });
+    const data = await response.json();
+    console.log(data);
+});
+
+server.listen(4000, () => {
+    console.log('server running on port 4000');
+});
